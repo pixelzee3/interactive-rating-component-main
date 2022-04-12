4d5011fd896da6208c5a0e3cb18f8089b6238122
pixelzee3 <bpixelzee@gmail.com>
2022-04-12 19:13:56 +0700

    Create rough layout of component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Rate from './Rate';
 
 function App() {
   return (
-    <div className="flex flex-col justify-center h-screen px-4">
+    <div className="flex flex-col justify-center h-screen px-4 bg-very-dark-blue">
       <Rate />
     </div>
   );
--- src/Rate.jsx
+++ src/Rate.jsx
@@ -1,7 +1,19 @@
+// TODO: Add star (star svg missing)
+
 const Rate = () => {
   return (
-    <div className="h-52 w-full bg-slate-300">
-      <h1>Hello World</h1>
+    <div className="h-52 w-full bg-dark-blue">
+      <span>***Insert star here***</span>
+      <h1 className="text-white">How did we do?</h1>
+      <p className="text-light-grey">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+      <div className="flex">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+      </div>
+      <button>Submit</button>
     </div>
   );
 };
