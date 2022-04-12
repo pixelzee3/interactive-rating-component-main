3329ccf4378237de9f6db2011a73c6f09edc2366
pixelzee3 <bpixelzee@gmail.com>
2022-04-12 19:00:45 +0700

    Create base component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Rate from './Rate';
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col justify-center h-screen px-4">
+      <Rate />
     </div>
   );
 }
---
+++ src/Rate.jsx
@@ -0,0 +1,9 @@
+const Rate = () => {
+  return (
+    <div className="h-52 w-full bg-slate-300">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default Rate;
