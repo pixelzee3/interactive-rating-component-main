b9cb5bb20eeedfc776f69894d6b538cd1c816b27
pixelzee3 <bpixelzee@gmail.com>
2022-04-12 19:11:33 +0700

    Fix tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
-    colors: {
-      primary: "hsl(25, 97%, 53%)",
-      "light-grey": "hsl(217, 12%, 63%)",
-      "medium-grey": "hsl(216, 12%, 54%)",
-      "dark-blue": "hsl(213, 19%, 18%)",
-      "very-dark-blue": "hsl(216, 12%, 8%)",
+    extend: {
+      colors: {
+        primary: 'hsl(25, 97%, 53%)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'medium-grey': 'hsl(216, 12%, 54%)',
+        'dark-blue': 'hsl(213, 19%, 18%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)',
+      },
     },
   },
   plugins: [],
