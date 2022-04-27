e7aba102b882b859ef5f97d2092c3fcf606123f9
pixelzee3 <bpixelzee@gmail.com>
2022-04-27 15:04:30 +0700

    Define theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsl(25, 97%, 53%)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'medium-grey': 'hsl(216, 12%, 54%)',
+        'dark-blue': 'hsl(213, 19%, 18%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)',
+      },
+    },
+    fontFamily: {
+      sans: ['Overpass', 'Inter var', 'sans-serif'],
+    },
   },
   plugins: [],
 };
