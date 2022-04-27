2647825054ae9b691ef928cabcc3433f986334b7
pixelzee3 <bpixelzee@gmail.com>
2022-04-27 15:40:44 +0700

    Define header and description components (font sizes not finalized nor responsive yet).

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
-  import Container from './components/Container.vue';
+import Container from './components/Container.vue';
+import Header from './components/Header.vue';
+import Description from './components/Description.vue';
 </script>
 
 <template>
   <div class="bg-darker-blue">
     <Container>
-      <img src="./assets/icon-star.svg" alt="">
+      <img src="./assets/icon-star.svg" alt="" />
     </Container>
-    <Container>
-      1
-    </Container>
+    <Container> 1 </Container>
+    <Header>How did we do?</Header>
+    <Description>
+      Please let us know how we did with your support request. All feedback is
+      appreciated to help us improve our offering!
+    </Description>
   </div>
 </template>
---
+++ src/components/Description.vue
@@ -0,0 +1,3 @@
+<template>
+  <p class="text-sm text-grey"><slot /></p>
+</template>
