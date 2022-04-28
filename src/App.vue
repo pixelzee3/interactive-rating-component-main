b842f82bb2c4997992247e6b296acaedeb017e49
pixelzee3 <bpixelzee@gmail.com>
2022-04-28 17:09:56 +0700

    Create card component.

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,23 @@
 import Container from './components/Container.vue';
 import Header from './components/Header.vue';
 import Description from './components/Description.vue';
+import Card from './components/Card.vue';
 </script>
 
 <template>
-  <div class="bg-darker-blue">
-    <Container>
-      <img src="./assets/icon-star.svg" alt="" />
-    </Container>
-    <Container> 1 </Container>
-    <Header>How did we do?</Header>
-    <Description>
-      Please let us know how we did with your support request. All feedback is
-      appreciated to help us improve our offering!
-    </Description>
+  <div
+    class="flex h-screen w-screen items-center justify-center bg-very-dark-blue p-4"
+  >
+    <Card>
+      <Container>
+        <img src="./assets/icon-star.svg" alt="" />
+      </Container>
+      <Header>How did we do?</Header>
+      <Description>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </Description>
+      <Container> 1 </Container>
+    </Card>
   </div>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-darker-blue w-full rounded-xl p-6">
+    <slot />
+  </div>
+</template>
