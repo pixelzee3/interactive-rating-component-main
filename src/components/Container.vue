6ec7bf8614b0db1deefb2e294cae0d94d5f7cf17
pixelzee3 <bpixelzee@gmail.com>
2022-04-28 17:54:55 +0700

    Style container states.

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -10,6 +10,12 @@ const props = defineProps({
 <template>
   <div
     class="flex aspect-square w-10 items-center justify-center rounded-full bg-dark-blue text-center text-xs text-grey"
+    :class="{
+      'cursor-pointer': clickable,
+      'hover:bg-slate-400': clickable,
+      'hover:text-white': clickable,
+      'active:bg-primary': clickable,
+    }"
   >
     <slot />
   </div>
