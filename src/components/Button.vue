0f421aa7e568c9e9f1a12f9995e60e31986f5c7d
pixelzee3 <bpixelzee@gmail.com>
2022-04-28 17:55:47 +0700

    Style button states.

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="w-full rounded-full bg-primary py-3 text-xs uppercase tracking-widest text-white"
+    class="w-full rounded-full bg-primary py-3 text-xs uppercase tracking-widest text-white active:bg-white active:text-primary"
   >
     <slot />
   </button>
