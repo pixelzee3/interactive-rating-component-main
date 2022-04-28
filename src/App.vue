bde28b50d5c0f924ffc09445bbcaf940e0b39400
pixelzee3 <bpixelzee@gmail.com>
2022-04-28 17:19:36 +0700

    Define button component.

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@ import Container from './components/Container.vue';
 import Header from './components/Header.vue';
 import Description from './components/Description.vue';
 import Card from './components/Card.vue';
+import Button from './components/Button.vue';
 </script>
 
 <template>
   <div
-    class="flex h-screen w-screen items-center justify-center bg-very-dark-blue p-4"
+    class="flex h-screen w-screen items-center justify-center bg-very-dark-blue px-6"
   >
     <Card>
       <Container>
@@ -18,16 +19,19 @@ import Card from './components/Card.vue';
       </div>
       <div class="mt-2">
         <Description>
-          Please let us know how we did with your support request. All feedback is
-          appreciated to help us improve our offering!
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
         </Description>
       </div>
-      <div class="flex justify-between mt-6">
+      <div class="mt-6 flex justify-between">
         <Container> 1 </Container>
         <Container> 2 </Container>
         <Container> 3 </Container>
         <Container> 4 </Container>
         <Container> 5 </Container>
+      </div>
+      <div class="mt-6">
+        <Button>Submit</Button>
       </div>
     </Card>
   </div>
---
+++ src/components/Button.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="w-full rounded-full bg-primary py-3 text-xs uppercase tracking-widest text-white"
+  >
+    <slot />
+  </button>
+</template>
