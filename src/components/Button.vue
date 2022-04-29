7dc820af29eb7aeef4cffccc3db0cd5b22d249c6
pixelzee3 <bpixelzee@gmail.com>
2022-04-29 22:36:55 +0700

    Build.

--- src/components/Button.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <button
-    class="w-full rounded-full bg-primary py-3 text-xs uppercase tracking-widest text-white active:bg-white active:text-primary"
-  >
-    <slot />
-  </button>
-</template>
--- src/components/Card.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="w-full max-w-xs rounded-xl bg-darker-blue p-6 md:rounded-3xl">
-    <slot />
-  </div>
-</template>
